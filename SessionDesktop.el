177cd40eceb1ccc4a457e437cfc931f5c6a2fe76
Joonki Hong <jkhong12000@gmail.com>
2017-11-30 23:52:54 +0900

    first commit

---
+++ SessionDesktop.el
@@ -0,0 +1,45 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Thu Nov 30 23:27:06 2017
+;; Desktop file format version 208
+;; Emacs version 25.1.1
+(setq revive:frame-configuration-to-restore
+  '(progn ))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (23072 5434 796249 0) (desktop . "208") "joonkihong@joonkis-iMac.local" nil nil ((((font-backend mac-ct) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (horizontal-scroll-bars) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 1) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (scroll-bar-height . 0) (alpha) (fullscreen) (fringe) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility) (frameset--id . "4126-66B2-DF1C-839B") (frameset--mini t . t) (height . 72) (width . 176) (modeline . t) (minibuffer . t) (unsplittable) (left . 0) (top . 23) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "joonkis-iMac.local") (explicit-name . t) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 6) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 30) (min-pixel-width-ignore . 38) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 1256) (pixel-height . 1148) (total-width . 179) (total-height . 72) (normal-height . 1.0) (normal-width . 1.0) (buffer " *empty*" (selected . t) (hscroll . 0) (fringes 1 8 nil) (margins nil) (scroll-bars nil 3 t nil 0 nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/cross_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/intro_yy.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "/Applications/Aquamacs.app/Contents/Resources/etc/COPYING" "/Applications/Aquamacs.app/Contents/Resources/etc/HELLO" "~/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/" "~/Dropbox/lanada_jk/Research/master depence/Dissertation/Double-MAC/main.t" "~/Dropbox/lanada_jk/Research/plb/longrange/main.tex" "/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/MAC_protocol.tex" "/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/routing_protocol.tex" "~/Library/Preferences/Aquamacs Emacs/customizations.el" "/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/intro_yy.tex" "/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/main.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/intro.tex" "/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/main.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/ma" "~/Dropbox/lanada_jk/Research/plb/longrange/conclusion.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/cross_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/routing_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/MAC_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/distributed_update.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/overview.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/related.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/intro.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/abstract.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/mymath.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/conclusion.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/cross_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/routing_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/MAC_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/distributed_update.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/overview.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/related.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/intro.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/abstract.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/mymath.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/conclusion.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/cross_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/routing_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/MAC_protocol.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/distributed_update.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/overview.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/related.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/intro.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/abstract.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/mymath.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/framework.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/MAC_protocol.tex" "/Users/joonkihong/Dropbox/EE205/2017/2017_project/Project4/documentation/proj4.tex" "~/Dropbox/EE205/2017/2017_project/Project4/documentation/proj4.tex" "~/Dropbox/EE205/2017/2017_project/Project4/documentation/edge_object.png" "~/Dropbox/EE205/2017/2017_project/Project4/documentation/" "~/Dropbox/EE205/2017/2017_project/Project4/documentation/proj4.tex" "~/Dropbox/EE205/2017/2017_project/format.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/" "~/Dropbox/lanada_jk/Research/plb/longrange/ls" "/Applications/Aquamacs.app/Contents/Resources/etc/TODO" "~/Dropbox/lanada_jk/Research/plb/longrange/overview.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/intro.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/imp_eval.tex" "~/Dropbox/lanada_jk/Research/plb/bird-mac/simulation.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/framework.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/related.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/analysis.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/main.tex" "~/Dropbox/lanada_jk/Research/plb/longrange/ref.bib"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 208
+  "/Users/joonkihong/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(visual-line-mode smart-spacing-mode TeX-PDF-mode)
+  2
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8-unix) (truncate-lines))
+  '((mark-ring nil)))
+
+(desktop-create-buffer 208
+  "/Users/joonkihong/Dropbox/lanada_jk/Research/plb/longrange/main.tex"
+  "main.tex"
+  'latex-mode
+  '(auto-fill-mode smart-spacing-mode TeX-PDF-mode bib-cite-minor-mode reftex-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((indent-tabs-mode) (buffer-file-coding-system . undecided-unix) (case-fold-search . t) (truncate-lines) (TeX-master . t))
+  '((mark-ring nil)))
+
---
+++ personal/custom.el
@@ -0,0 +1,61 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
+ '(TeX-source-correlate-method (quote synctex))
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(package-selected-packages
+   (quote
+    (use-package dumb-jump auctex dirtree vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(autoload 'dirtree "dirtree" "Add directory to tree view" t)
+(global-set-key (kbd "M-SPC") (lambda() (interactive) (push-mark nil nil 1)))
+
+
+;; AucTeX
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-PDF-mode t)
+
+;; Use Skim as viewer, enable source <-> PDF sync
+;; make latexmk available via C-c C-c
+;; Note: SyncTeX is setup via ~/.latexmkrc (see below)
+(add-hook 'LaTeX-mode-hook (lambda ()
+  (push
+    '("latexmk" "latexmk -pdf %s" TeX-run-TeX nil t
+      :help "Run latexmk on file")
+    TeX-command-list)))
+(add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
+
+;; use Skim as default pdf viewer
+;; Skim's displayline is used for forward search (from .tex to .pdf)
+;; option -b highlights the current line; option -g opens Skim in the background  
+(setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
+(setq TeX-view-program-list
+     '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
+
+
+(use-package dumb-jump
+             :bind (("M-g o" . dumb-jump-go-other-window)
+                    ("M-g j" . dumb-jump-go)
+                    ("M-g q" . dumb-jump-quick-look)
+                    ("M-g b" . dumb-jump-back)
+                    ("M-g i" . dumb-jump-go-prompt)
+                    ("M-g x" . dumb-jump-go-prefer-external)
+                    ("M-g z" . dumb-jump-go-prefer-external-other-window))
+             :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+             :ensure)
